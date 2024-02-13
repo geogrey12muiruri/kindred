5b78082c6bd1d7a58e84da4da4b3eb56f4727993
awadh <awadhhassan566@gmail.com>
2024-02-13 15:20:53 +0300

    login updates

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -2,7 +2,28 @@
  
  function Login() {
    return (
-     <div>Login</div>
+     <div>
+
+        <div>
+            <h1>login</h1>
+            <div>
+                <input type="email" />
+                <label htmlfor="">your email</label>
+            </div>
+            <div>
+                <input type="email" />
+                <label htmlfor="">your email</label>
+            </div>
+            <div>
+                <div>
+                <input type="checkbox" name="" id="" />
+                <label htmlfor="">remember me</label>
+            </div>
+            <span>Forgot password?</span>
+            </div>
+            <button>login</button>
+        </div>
+     </div>
    )
  }
  
