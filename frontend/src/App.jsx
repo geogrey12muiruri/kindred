b98940a4de7bc29e20a5d2874c6bbc5ad43a5c50
awadh <awadhhassan566@gmail.com>
2024-02-13 14:40:25 +0300

    adding  routes on app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/nav/Navbar"
 import Home from "./components/home/Home"
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 function App() {
  
 
@@ -9,6 +11,8 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
+          <Login />
+          <Register />
           <Route path='/' element={<Home />} />
         </Routes>
       </BrowserRouter>
